05337ac3a769348fe0ee514bd4be1a661a725add
Bimal Adhikari <bimaladhikari175@gmail.com>
2025-09-11 21:36:25 +0200

    Opdracht de eerste route

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,17 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/planeten', function () {
+    return [
+        "Uranus",
+        "Jupiter",
+        "Mars",
+        "Aarde",
+        "Saturnus",
+        "Pluto",
+        "Neptunus",
+        "Venus"
+    ];
 });
+
+?>
