d1cb1dc32dc2aa64eae01d5dc845a67eee9a0c16
Bimal Adhikari <bimaladhikari175@gmail.com>
2025-10-12 10:54:01 +0200

    Opdracht controller maken met Artisan

--- routes/web.php
+++ routes/web.php
@@ -1,54 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\PlanetController;
+use App\Http\Controllers\PlanetController; // <-- voeg dit hier toe!
 
-// Overzicht van alle planeten
-Route::get('/planets', [PlanetController::class, 'index'])->name('planets.index');
+// Routes voor planeten
+Route::get('/planets', [PlanetController::class, 'index']);
+Route::get('/planets/{planet}', [PlanetController::class, 'show']);
 
-// Detailpagina van een specifieke planeet
-Route::get('/planets/{planet}', [PlanetController::class, 'show'])->name('planets.show');
- 
- 
-namespace App\Http\Controllers;
-
-use Illuminate\Http\Request;
-
-class PlanetController extends Controller
-{
-    public function index()
-    {
-        $planets = [
-            'mercury' => ['name' => 'Mercurius', 'description' => '...'],
-            'venus' => ['name' => 'Venus', 'description' => '...'],
-            'earth' => ['name' => 'Aarde', 'description' => '...'],
-            'mars' => ['name' => 'Mars', 'description' => '...'],
-            // ... andere planeten
-        ];
-        
-        return view('planets.index', compact('planets'));
-    }
-
-    public function show($planet)
-    {
-        $planets = [
-            'mars' => [
-                'name' => 'Mars',
-                'description' => 'Mars is de vierde planeet vanaf de Zon...',
-                'diameter' => '6.779 km',
-                'distance_from_sun' => '227,9 miljoen km',
-                // ... meer data
-            ],
-            // ... andere planeten
-        ];
-
-        if (!array_key_exists($planet, $planets)) {
-            abort(404, 'Planeet niet gevonden');
-        }
-
-        return view('planets.show', [
-            'planet' => $planets[$planet],
-            'planetSlug' => $planet
-        ]);
-    }
-}
+// Optioneel: homepage
+Route::get('/', function () {
+    return view('welcome');
+});
