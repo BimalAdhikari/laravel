677251154439ba667ecc43aa0af2e13c2ee61c14
Bimal Adhikari <bimaladhikari175@gmail.com>
2025-11-30 23:40:09 +0100

    Opdracht eerste query

--- database/migrations/create_planets_table.php
+++ database/migrations/create_planets_table.php
@@ -3,29 +3,42 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
-    public function up()
-    {
-        Schema::create('planets', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('description');
-            $table->integer('size_in_km');
-            $table->timestamps();
-        });
+    
+     
+
+  public function up(): void{Schema::create('planets', function (Blueprint $table) {$table->id();$table->string('name');$table->string('description');$table->integer('size_in_km');$table->timestamps();});
+
+        DB::table('planets')->insert([
+            [
+                'name' => 'Uranus', 
+                'description' => 'De Blauwe planeet', 
+                'size_in_km' => 69420, 
+                'created_at' => now(), 
+                'updated_at' => now()
+            ],
+            [
+                'name' => 'De zon', 
+                'description' => 'De heetste ster', 
+                'size_in_km' => 13920, 
+                'created_at' => now(), 
+                'updated_at' => now()
+            ],
+            [
+                'name' => 'Earth', 
+                'description' => 'Our home planet', 
+                'size_in_km' => 12742, 
+                'created_at' => now(), 
+                'updated_at' => now()
+            ],
+        ]);
     }
 
+    
+     
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
-    {
-        Schema::dropIfExists('planets');
-    }
+  public function down(): void{Schema::dropIfExists('planets');}
 };
